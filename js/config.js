f6cda8a79ecf09dcd135986071b2e0075788ff28
Teddz <teddzfr@gmail.com>
2026-08-20 20:14:16 +0800

    Set live Supabase project URL and publishable key

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.REVIEWS_CONFIG = {
-  supabaseUrl: "PASTE_YOUR_SUPABASE_URL_HERE",
-  supabaseAnonKey: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  supabaseUrl: "https://poeqfaqjafkulunksxpm.supabase.co",
+  supabaseAnonKey: "sb_publishable_m912Y2SrNCiu6QOtIZ_Nkw_bZ6rzcC0",
   storageBucket: "review-files",
   minDescLength: 80,
   maxImageMb: 5,
